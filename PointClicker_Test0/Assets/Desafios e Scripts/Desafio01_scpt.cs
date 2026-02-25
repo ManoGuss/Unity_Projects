e277932a9c38fa711906d5b8d008403c1cf9a5be
Gustavo Silva <gmanogus@gmail.com>
2026-02-25 15:49:33 -0300

    Desafio01 concluído GmOBJ_Player_position
    nesse desafio eu construí um código que cria um GmOBJ de referencia sendo o player, para se mover até as coordenadas definidas, pra isso tive que trabalhar com o Public GmOBJ

---
+++ PointClicker_Test0/Assets/Desafios e Scripts/Desafio01_scpt.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Desafio01_scpt: MonoBehaviour
+{
+
+
+    public GameObject player;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        //player = GetComponent<GameObject>();
+        player.transform.position = new Vector3(1, 1, 1);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+
+
+
+    }
+}
